44f620b5876ed5dcf44a6c9e233bbbc5422f5e8f
SherwinShen <306407158@qq.com>
2021-05-03 17:55:35 +0800

    chore: 依赖更新

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const resolve = require('rollup-plugin-node-resolve') // 可以将第三方模块打包进项目里，这样即使不安装第三方模块也能使用
-const commonjs = require('rollup-plugin-commonjs') // 可以将commonjs模块打包
-const babel = require('rollup-plugin-babel') // 将es6语法转es5
-const json = require('rollup-plugin-json') // 打包json文件
-const vue = require('rollup-plugin-vue') // 打包vue文件
-const postcss = require('rollup-plugin-postcss') // 支持css预处理
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
+import vue from 'rollup-plugin-vue'
+import postcss from 'rollup-plugin-postcss'
 
 const inputPath = path.resolve(__dirname, './src/index.js') // 入口地址
 const outputUmdPath = path.resolve(__dirname, './dist/datav.js') // 输出地址 - umd模块
 const outputEsPath = path.resolve(__dirname, './dist/datav.es.js') // 输出地址 - es模块
 
-module.exports = {
+const config = {
   input: inputPath,
   output: [
     {
@@ -31,22 +31,30 @@ module.exports = {
     }
   ],
   plugins: [
+    // 打包vue文件
     vue(),
+    // 可以将第三方模块打包进项目里，这样即使不安装第三方模块也能使用
     resolve({preferBuiltins: true}),
+    // 可以将commonjs模块打包
     commonjs(),
+    // 将es6语法转es5
     babel({
       exclude: 'node_modules/**',
-      runtimeHelpers: true,
+      babelHelpers: 'runtime',
       plugins: [
         ['@babel/transform-runtime', {
           regenerator: true
         }]
       ]
     }),
+    // 打包json文件
     json(),
+    // 支持css预处理
     postcss({
       plugins: []
     })
   ],
   external: ['vue', 'echarts'] // 外部引用模块，即使有resolve插件也会被引入到外部，不会打包进入
 }
+
+export default config
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const resolve = require('rollup-plugin-node-resolve') // 可以将第三方模块打包进项目里，这样即使不安装第三方模块也能使用
-const commonjs = require('rollup-plugin-commonjs') // 可以将commonjs模块打包
-const babel = require('rollup-plugin-babel') // 将es6语法转es5
-const json = require('rollup-plugin-json') // 打包json文件
-const vue = require('rollup-plugin-vue') // 打包vue文件
-const postcss = require('rollup-plugin-postcss') // 支持css预处理
-const {terser} = require('rollup-plugin-terser') // 打包压缩
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
+import json from '@rollup/plugin-json'
+import vue from 'rollup-plugin-vue'
+import postcss from 'rollup-plugin-postcss'
+import {terser} from "rollup-plugin-terser";
 
 const inputPath = path.resolve(__dirname, './src/index.js') // 入口地址
 const outputUmdPath = path.resolve(__dirname, './dist/datav.js') // 输出地址 - umd模块
 const outputEsPath = path.resolve(__dirname, './dist/datav.es.js') // 输出地址 - es模块
 
-module.exports = {
+const config = {
   input: inputPath,
   output: [
     {
@@ -32,23 +32,32 @@ module.exports = {
     }
   ],
   plugins: [
-    resolve(),
+    // 打包vue文件
+    vue(),
+    // 可以将第三方模块打包进项目里，这样即使不安装第三方模块也能使用
+    resolve({preferBuiltins: true}),
+    // 可以将commonjs模块打包
     commonjs(),
+    // 将es6语法转es5
     babel({
       exclude: 'node_modules/**',
-      runtimeHelpers: true,
+      babelHelpers: 'runtime',
       plugins: [
         ['@babel/transform-runtime', {
           regenerator: true
         }]
       ]
     }),
+    // 打包json文件
     json(),
-    vue(),
+    // 支持css预处理
     postcss({
       plugins: []
     }),
-    terser(),
+    // 打包压缩
+    terser()
   ],
   external: ['vue', 'echarts'] // 外部引用模块，即使有resolve插件也会被引入到外部，不会打包进入
 }
+
+export default config
